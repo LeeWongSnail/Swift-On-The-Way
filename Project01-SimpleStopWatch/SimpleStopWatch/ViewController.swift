d2cc536d4a6c634d05436da07e21ef70dff18a10
LeeWongSnail <@wangli_0632@163.com>
2016-02-19 18:17:40 +0800

    simpleStopWatch

---
+++ Project01-SimpleStopWatch/SimpleStopWatch/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  SimpleStopWatch
+//
+//  Created by LeeWong on 16/2/19.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    private let timerInterval = 0.1
+    
+    private var isPlaying = false
+    
+    var timer : NSTimer?
+    
+    private func addTimer() {
+      timer = NSTimer.scheduledTimerWithTimeInterval(timerInterval, target: self, selector:"changeTime", userInfo: nil, repeats: true)
+        
+    }
+    
+    //这里注意 在使用selector调用某个私有方法的时候 必须在前面加上@objc
+    @objc private func changeTime() {
+        timeLabel.text = "\(Double(timeLabel.text!)! + timerInterval)"
+    }
+    
+    @IBAction func pause() {
+        timer?.invalidate()
+        isPlaying = false
+    }
+
+    @IBAction func play() {
+        if !isPlaying {
+            addTimer()
+            isPlaying = true
+        }
+        
+    }
+    
+    @IBAction func reset() {
+        timeLabel.text = "0.0"
+        isPlaying = false
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if timer == nil {
+            addTimer()
+        }
+    }
+}
+
---
+++ Project01-SimpleStopWatch/SimpleStopWatchTests/SimpleStopWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleStopWatchTests.swift
+//  SimpleStopWatchTests
+//
+//  Created by LeeWong on 16/2/19.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleStopWatch
+
+class SimpleStopWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
