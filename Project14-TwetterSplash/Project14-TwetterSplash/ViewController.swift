484de7fb92056a9eb93da75cfee46c1c759cf52d
LeeWongSnail <@wangli_0632@163.com>
2016-02-26 16:18:42 +0800

    Twetter Splash

---
+++ Project14-TwetterSplash/Project14-TwetterSplash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project14-TwetterSplash
+//
+//  Created by LeeWong on 16/2/26.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
